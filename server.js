b33462f2ea97633146fc05267c50240cf1668de1
JJ <jjcasey4@hotmail.com>
2023-04-21 08:50:46 +0100

    removed git keep

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const { execPythonCode, callOpenAI } = require('./helpers');
 
 const app = express();
 
@@ -17,3 +21,30 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+app.use(bodyParser.json());
+
+app.post('/execute', async (req, res) => {
+    const { code, language, stdin } = req.body;
+  
+    if (language === 'python') {
+      try {
+        const output = await execPythonCode(code, stdin);
+        res.json({ output });
+      } catch (error) {
+        res.json({ error: error.toString() });
+      }
+    } else {
+      res.json({ error: 'Invalid language' });
+    }
+  });
+  
+app.post('/openai', async (req, res) => {
+    const { prompt } = req.body;
+  
+    try {
+      const text = await callOpenAI(prompt);
+      res.json({ text });
+    } catch (error) {
+      res.json({ error: error.toString() });
+    }
+});
