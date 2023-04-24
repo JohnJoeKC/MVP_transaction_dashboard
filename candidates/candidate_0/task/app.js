b37fc9a5b69fb04347618fef5f620a8911c640e5
JohnJoeKC <130591431+JohnJoeKC@users.noreply.github.com>
2023-04-24 18:23:18 +0100

    undo

--- candidates/candidate_0/task/app.js
+++ candidates/candidate_0/task/app.js
@@ -1,56 +1,20 @@
-// Sample transaction data
-const transactions = [
-  { id: 1, date: "2023-04-10", amount: 50, paymentMethod: "Credit Card" },
-  { id: 2, date: "2023-04-12", amount: 30, paymentMethod: "Cash" },
-  { id: 3, date: "2023-04-15", amount: 120, paymentMethod: "Debit Card" },
-  { id: 4, date: "2023-04-20", amount: 75, paymentMethod: "Credit Card" },
-];
+const fetchData = async () => {
+  // Fetch data from the API endpoint
+};
 
-// Create a table displaying the transaction data
-function createTransactionTable(transactions) {
-  const table = document.createElement("table");
-  table.classList.add("table");
 
-  const thead = document.createElement("thead");
-  thead.innerHTML = `
-    <tr>
-      <th>ID</th>
-      <th>Date</th>
-      <th>Amount</th>
-      <th>Payment Method</th>
-    </tr>
-  `;
-  table.appendChild(thead);
+const processData = (data) => {
+  // Process data to calculate total transactions, total value, and average value
+};
 
-  const tbody = document.createElement("tbody");
-  transactions.forEach(transaction => {
-    const tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>${transaction.id}</td>
-      <td>${transaction.date}</td>
-      <td>${transaction.amount}</td>
-      <td>${transaction.paymentMethod}</td>
-    `;
-    tbody.appendChild(tr);
-  });
-  table.appendChild(tbody);
 
-  return table;
-}
+const createDashboard = (data) => {
+  // Create the dashboard HTML structure and render the data
+};
 
-// Render the transaction dashboard
-function renderDashboard() {
-  const container = document.createElement("div");
-  container.classList.add("container");
 
-  const title = document.createElement("h1");
-  title.textContent = "Transaction Dashboard";
-  container.appendChild(title);
-
-  const table = createTransactionTable(transactions);
-  container.appendChild(table);
-
-  return container;
-}
-
-document.getElementById("app").appendChild(renderDashboard());
+(async () => {
+  const data = await fetchData();
+  const processedData = processData(data);
+  createDashboard(processedData);
+})();
