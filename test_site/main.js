503b00bc0490acbb1f6fcb39bc987ea74e5784e2
JJ <jjcasey4@hotmail.com>
2023-04-26 14:23:30 +0100

    main js to finx monaco editor missing

--- test_site/main.js
+++ test_site/main.js
@@ -1,64 +1,52 @@
 let editor;
 
 function initMonacoEditor() {
-  require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.30.1/min/vs' }});
-  require(['vs/editor/editor.main'], function() {
+  require.config({
+    paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.30.1/min/vs' },
+  });
+
+  require(['vs/editor/editor.main'], function () {
     editor = monaco.editor.create(document.getElementById('monaco-editor-container'), {
-      value: "",
-      language: "plaintext",
-      theme: "vs-dark",
+      value: 'print("Hello, world!")',
+      language: 'python',
+      theme: 'vs-dark',
       automaticLayout: true,
     });
+
+    // Call fetchAndPopulateFilesDirectly() once, after editor initialization
+    fetchAndPopulateFilesDirectly();
   });
 }
 
 initMonacoEditor();
 
-require.config({
-  paths: {
-    'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.30.1/min/vs'
-  }
-});
-
-require(['vs/editor/editor.main'], function () {
-  editor = monaco.editor.create(document.getElementById('monaco-editor-container'), {
-    value: [
-      'print("Hello, world!")',
-    ].join('\n'),
-    language: 'python'
-  });
-
-  editor.updateOptions({ theme: 'vs-dark' });
-
-  // Call fetchAndPopulateFilesDirectly() once, after editor initialization
-  fetchAndPopulateFilesDirectly();
-});
-
 async function fetchAndPopulateFilesDirectly() {
-  const repoOwner = "JohnJoeKC";
-  const repoName = "MVP_transaction_dashboard";
-  const taskFolderPath = "candidates/candidate_0/task";
+  const repoOwner = 'JohnJoeKC';
+  const repoName = 'MVP_transaction_dashboard';
+  const taskFolderPath = 'candidates/candidate_0/task';
 
   try {
-    const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${taskFolderPath}`);
+    const response = await fetch(
+      `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${taskFolderPath}`,
+    );
     const files = await response.json();
     populateFileSelect(files);
   } catch (error) {
-    console.error("Error fetching repository content:", error);
+    console.error('Error fetching repository content:', error);
   }
 }
 
 function populateFileSelect(files) {
-  const fileSelect = document.getElementById("file-select");
+  const fileSelect = document.getElementById('file-select');
 
-  files.forEach(file => {
-    const option = document.createElement("option");
+  files.forEach((file) => {
+    const option = document.createElement('option');
     option.value = file.path;
     option.textContent = file.name;
     fileSelect.appendChild(option);
   });
 
-  fileSelect.addEventListener("change", async function () {
+  fileSelect.addEventListener('change', async function () {
     const filePath = this.value;
     const fileContent = await fetchFileContent(filePath);
     editor.setValue(fileContent);
@@ -66,15 +54,17 @@ function populateFileSelect(files) {
 }
 
 async function fetchFileContent(filePath) {
-  const repoOwner = "JohnJoeKC";
-  const repoName = "MVP_transaction_dashboard";
+  const repoOwner = 'JohnJoeKC';
+  const repoName = 'MVP_transaction_dashboard';
 
   try {
-    const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`);
+    const response = await fetch(
+      `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`,
+    );
     const file = await response.json();
     const fileContent = atob(file.content);
     return fileContent;
   } catch (error) {
-    console.error("Error fetching file content:", error);
+    console.error('Error fetching file content:', error);
   }
 }
